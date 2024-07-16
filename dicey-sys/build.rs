2222d337909684569d336c139c42ad33ac830f53
Marco Cilloni <marco.cilloni@zuru.com>
2024-07-16 11:58:47 +0200

    modules: remove redundant gitmodules

--- dicey-sys/build.rs
+++ dicey-sys/build.rs
@@ -35,3 +35,54 @@ fn main() {
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
 }
+
+pub fn build_zlib_ng(target: &str, compat: bool) {
+    let mut cmake = cmake::Config::new("src/zlib-ng");
+    
+    cmake
+        .define("BUILD_SHARED_LIBS", "OFF")
+        .define("ZLIB_COMPAT", if compat { "ON" } else { "OFF" })
+        .define("ZLIB_ENABLE_TESTS", "OFF")
+        .define("WITH_GZFILEOP", "ON");
+
+    let install_dir = cmake.build();
+
+    let includedir = install_dir.join("include");
+    let libdir = install_dir.join("lib");
+    let libdir64 = install_dir.join("lib64");
+
+    println!(
+        "cargo:rustc-link-search=native={}",
+        libdir.to_str().unwrap()
+    );
+
+    println!(
+        "cargo:rustc-link-search=native={}",
+        libdir64.to_str().unwrap()
+    );
+
+    let mut debug_suffix = "";
+
+    let libname = if target.contains("windows") && target.contains("msvc") {
+        if env::var("OPT_LEVEL").unwrap() == "0" {
+            debug_suffix = "d";
+        }
+        "zlibstatic"
+    } else {
+        "z"
+    };
+
+    println!(
+        "cargo:rustc-link-lib=static={}{}{}",
+        libname,
+        if compat { "" } else { "-ng" },
+        debug_suffix,
+    );
+
+    println!("cargo:root={}", install_dir.to_str().unwrap());
+    println!("cargo:include={}", includedir.to_str().unwrap());
+
+    if !compat {
+        println!("cargo:rustc-cfg=zng");
+    }
+}
